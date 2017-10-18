f38c1af8d83be4e74e2e1697e1061d81d3fb4fbf
apoorva.joshi <apoorva.joshi@v2solutions.com>
2017-10-18 12:56:03 +0530

    Created POC app for Chatto. Integrated Chatto using Carthage. Added Chatto and ChattoAdditions framework in Project file.

---
+++ V2POC_Chatto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  V2POC_Chatto
+//
+//  Created by apoorva on 18/10/17.
+//  Copyright © 2017 apoorva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
