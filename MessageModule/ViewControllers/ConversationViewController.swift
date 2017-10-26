f9938444a12c139e1ebfbe23cbe57c7940bd0732
apoorva.joshi <apoorva.joshi@v2solutions.com>
2017-10-26 16:10:48 +0530

    Added presenter in ConversationViewController and made other changes accordingly

--- MessageModule/ViewControllers/ConversationViewController.swift
+++ MessageModule/ViewControllers/ConversationViewController.swift
@@ -12,8 +12,63 @@ import ChattoAdditions
 
 class ConversationViewController: BaseChatViewController {
 
+    var messageSender: FakeMessageSender!
+    var chatInputPresenter: BasicChatInputBarPresenter!
+    var dataSource: FakeDataSource! {
+        didSet {
+            self.chatDataSource = self.dataSource
+        }
+    }
+    
+    lazy private var baseMessageHandler: BaseMessageHandler = {
+        return BaseMessageHandler(messageSender: self.messageSender)
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        // dataSource and messageSender need to be initialised here
+        let pageSize = 50
+        dataSource = FakeDataSource(messages: [], pageSize: pageSize)
+        self.messageSender = dataSource.messageSender
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "V2 Conversations"
+        super.chatItemsDecorator = ChatItemsDemoDecorator()
     }
+    
+    override func createChatInputView() -> UIView {
+        let chatInputView = ChatInputBar.loadNib()
+        var appearance = ChatInputBarAppearance()
+        appearance.sendButtonAppearance.title = NSLocalizedString("Send", comment: "")
+        appearance.textInputAppearance.placeholderText = NSLocalizedString("Type a message", comment: "")
+        self.chatInputPresenter = BasicChatInputBarPresenter(chatInputBar: chatInputView, chatInputItems: self.createChatInputItems(), chatInputBarAppearance: appearance)
+        chatInputView.maxCharactersCount = 1000
+        return chatInputView
+    }
+    
+    func createChatInputItems() -> [ChatInputItemProtocol] {
+        var items = [ChatInputItemProtocol]()
+        items.append(self.createTextInputItem())
+        return items
+    }
+    
+    private func createTextInputItem() -> TextChatInputItem {
+        let item = TextChatInputItem()
+        item.textInputHandler = { [weak self] text in
+            self?.dataSource.addTextMessage(text)
+        }
+        return item
+    }
+    
+    // A collection of Presenter Builders are given to the ConversationViewController to configure the cells
+    
+    override func createPresenterBuilders() -> [ChatItemType : [ChatItemPresenterBuilderProtocol]] {
+        let textMessagePresenter = TextMessagePresenterBuilder(viewModelBuilder: TextMessagesViewModelBuilder(), interactionHandler: TextMessagesHandler(baseHandler: self.baseMessageHandler))
+        
+        return [DemoTextMessageModel.chatItemType: [textMessagePresenter]]
+        
+    }
+
 }
